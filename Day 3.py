d8fd44de403dd216177ad288ce69853943db0ab0
Rashid3601 <rashidsadi55@gmail.com>
2021-06-16 16:03:27 +0100

    Add files via upload

---
+++ Day 3.py
@@ -0,0 +1,36 @@
+Num = 17
+if Num%5==0 and Num%3==0:
+    print("Fizzbuzz")
+elif Num%5==0:
+    print("Buzz")
+elif Num%3==0:
+    print("Fizz")
+else: 
+    print(Num)
+print("Enter a number")
+x = input()
+print(int(x) + 10)  # by using the int() in built method, 
+                    #you turn a string (like when we use "input()") 
+                    # into an integer (i.e. a number)
+#this is an example of using the in built int() method
+
+#the int method turns stings into numbers]
+
+#we need this for calculations if we use the input method
+
+#because, when you use input() python reads the input() as a string by default!!
+#time = 7
+
+if time = 7:
+    print("I'm at home")
+elif time = 8:
+    print("I'm commuting")
+elif time = 9:
+    print("I'm at work")
+else:
+    print("Enter I'm chilling")
+introducing functions
+
+
+
+
